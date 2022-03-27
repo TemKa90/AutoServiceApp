64e0d971447fee9cbeb3829a9cbbf8fd38159b2f
immynitet <megusta5442@mail.ru>
2022-03-27 16:49:45 +0500

    ВСЁ

---
+++ AutoServiceApp/model/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoServiceApp.model
+{
+    public class Core
+    {
+        public AutoServiceEntities context = new AutoServiceEntities();
+    }
+}
